bd54bf71e3349a1c404b7c7eeb687bf872222312
Muhammet ismail ardıç <m.ardic22@hotmail.com>
2023-09-19 23:44:07 +0300

    latest changes pushed

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -3,9 +3,6 @@ using Discount.Grpc.Entities;
 using Discount.Grpc.Protos;
 using Discount.Grpc.Repositories;
 using Grpc.Core;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
 namespace Discount.Grpc.Services
 {
